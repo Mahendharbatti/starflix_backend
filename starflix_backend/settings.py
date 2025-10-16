78fbb7354496f39bc514bb25dc5aaf6ae6cd12e3
mahendharbatti <mahendharbatti@gmail.com>
2025-10-16 22:33:11 +0530

    url change

--- starflix_backend/settings.py
+++ starflix_backend/settings.py
@@ -149,5 +149,5 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 AUTH_USER_MODEL = 'accounts.CustomUser'
 CORS_ALLOWED_ORIGINS = [
-    'https://starflix-movies.netlify.app',  # Allow React frontend
+    'https://starflix-movies.netlify.app','starflix-movies.netlify.app',  # Allow React frontend
 ]
